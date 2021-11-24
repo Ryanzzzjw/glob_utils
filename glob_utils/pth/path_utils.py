8a84740bc829d939c8ab5689e73c7c1e272e974a
David Metz <david.metz67@gmail.com>
2021-11-24 19:05:20 +0100

    name changes

--- glob_utils/pth/path_utils.py
+++ glob_utils/pth/path_utils.py
@@ -302,9 +302,9 @@ def get_dir(title:str='Select a directory', initialdir:str=None)->str:
 
 
 if __name__ == "__main__":
-    from glob_utils.log.log  import change_level, main_log
+    from glob_utils.log.log  import change_level_logging, main_log
     import logging
     main_log()
-    change_level(logging.DEBUG)
+    change_level_logging(logging.DEBUG)
 
     
