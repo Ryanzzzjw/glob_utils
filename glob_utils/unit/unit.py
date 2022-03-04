41c3b5ce0374a309da7cfddfaaf21d13b893e2b5
David Metz <david.metz67@gmail.com>
2022-03-04 10:16:54 +0100

    refactoring of engineer representation

--- glob_utils/unit/unit.py
+++ glob_utils/unit/unit.py
@@ -1,105 +1,108 @@
 import re
-from tokenize import String
+from typing import Tuple, Union
 import numpy as np
 
-def unit_converter(var: any):
-    """ This is the main function of unit conversion.
+# prefixe table with the corresping exponent ( 10^exp) as key
+UNIT_PREFIX = {
+    -24 : 'y', # yokto
+    -21 : 'z', # zepto
+    -18 : 'a', # atto
+    -15 : 'f', # femto
+    -12 : 'p', # pico
+    -9  : 'n', # nano
+    -6  : 'u', # micro
+    -3  : 'm', # milli
+    0   : '',  # 
+    3   : 'k', # kilo
+    6   : 'M', # mega
+    9   : 'G', # giga
+    12  : 'T', # tera 
+    15  : 'P', # peta
+    18  : 'E', # exa
+    21  : 'Z', # zetta
+    24  : 'Y'  # yotta
+    } 
+    
+
+# def unit_converter_str(var,format_str:str='.2f')-> str:
+#     """ Convert a float in a str with a unit and a prefix .
+#         such as  val=1000, default_unit = 'Hz' > '1.0 kHz'
+
+#     Args:
+#         val (float): Value to convert
+#         default_unit (str): unit in which the value is expressed
+
+#     Returns:
+#         str: String with converted value and unit
+#     """
+#     variable = re.findall(r'[A-Za-z]+|\d+\.?\d*', var)
+#     number, unit = float(variable[0]), variable[1]
+#     return eng(number, unit, format_str)
+
+
+def eng(x:Union[float, int], unit:str='', format_str:str='.4g')-> str:
+    """Return a string representing x in an engineer friendly notation with a unit and a prefix .
+       such as  val=1000, unit = 'Hz' > '1.000 kHz'
 
     Args:
-        var (any): 1. value correspoding to the non-prefix unit
-                   2. including both value and non-prefix unit (e.g. 100A or 100 A)
+        x (Union[float, int]): value to represent
+        unit (str, optional): unit in which x is expressed. Defaults to ''.
+        format_str (str, optional): format of the value. Defaults to '.4g'.
+
+    Raises:
+        TypeError: raise in case x is not an int or a float
 
     Returns:
-        _type_: converted value and unit
+        str: represention of x in an engineer friendly notation with a unit and a prefix .
     """
-    if isinstance(var, (int, float)):
-        number = float(var)
-        output = unit_converter_value(number)
-    elif isinstance (var, str):
-        variable = re.findall(r'[A-Za-z]+|\d+\.?\d*', var)
-        number, default_unit = float(variable[0]), variable[1]
-        output = unit_converter_str(number, default_unit)
-        
-    return output
     
+    if ~isinstance(x, (int, float)):
+        raise TypeError('x should be an integer or a float')
 
-def unit_converter_str(val: float, default_unit: str):
-    """ This function is used to convert the input including both value and non-prefix unit .
+    x= float(x)
+    a,exp = powerise10(x)
+
+    if -3 < exp < 3:
+        new_val,exp3= x, 0
+    new_val = a * 10**(exp % 3)
+    exp3 = exp - exp % 3
+
+    if exp3 not in list(UNIT_PREFIX.keys()):
+        new_val=x
+        exp3= 0
+
+    print(a, exp3)
+    return f'{str(format(new_val, format_str))} {UNIT_PREFIX[exp3]}{unit}'
+
+def powerise10(x:float)->Tuple[float, int]:
+    """Returns a number x as a*10**exp with 0 <= a < 10
 
     Args:
-        val (float): _description_
-        default_unit (str): _description_
+        x (float): number to 
 
     Returns:
-        _type_: String with converted value and unit
-    """
-    unit_prefix = {-9: 'n', -6: 'u', -3: 'm', 0: '', 3: 'k', 6: 'M', 9: 'G'}
-    
-    exponent = np.floor(np.log10(np.abs(val))).astype(int)
-    if exponent < 3 and exponent > -3:
-        output = str(val) + ' ' + default_unit
-    
-    elif exponent >= 3:
-        
-        if exponent % 3  == 0:
-            output = str(val / (1000 **(exponent/3))) + ' ' + unit_prefix[exponent] + default_unit
-        else:
-            output = str(format(val / (1000 **(exponent//3)), '.2f')) + ' ' + unit_prefix[exponent - exponent % 3] + default_unit
-    
-    else:
-        if exponent % 3 == 0:
-            output = str(val * (1000**(np.abs(exponent)/3))) + ' ' + unit_prefix[exponent] + default_unit
-        else:
-            output = str(format((val * (1000**(np.abs(exponent)//3))), '.2f')) + ' ' + unit_prefix[exponent + np.abs(exponent) % 3] + default_unit     
-        
-    return output
-
-def unit_converter_value(val: float):
-    """ This function is used to convert the input including only value
-
-    Args:
-        val (float): _description_
-
-    Returns:
-        _type_: String with converted value and new prefix
+        Tuple[float, int]: a and exp
     """
     
-    unit_prefix = {-9: 'n', -6: 'u', -3: 'm', 0: '', 3: 'k', 6: 'M', 9: 'G'}
-    
-    exponent = np.floor(np.log10(np.abs(val))).astype(int)
-    if exponent < 3 and exponent > -3:
-        output = val
-    
-    elif exponent >= 3:
-        
-        if exponent % 3  == 0:
-            output = str(val / (1000 **(exponent/3))) + ' ' + unit_prefix[exponent] 
-        else:
-            output = str(format(val / (1000 **(exponent//3)), '.2f')) + ' ' + unit_prefix[exponent - exponent % 3] 
-    
-    else:
-        if exponent % 3 == 0:
-            output = str(val * (1000**(np.abs(exponent)/3))) + ' ' + unit_prefix[exponent] 
-        else:
-            output = str(format((val * (1000**(np.abs(exponent)//3))), '.2f')) + ' ' + unit_prefix[exponent + np.abs(exponent) % 3]   
-        
-    return output
+    if x == 0:
+        return 0,0
+    sign= np.sign(x)
+    x= np.abs(x)
+    a = 1.0 * x / 10**(np.floor(np.log10(x)))
+    exp = int(np.floor(np.log10(x)))
+    return sign*a,exp
 
 
 if __name__ == "__main__":
+    from random import randint, random
     from glob_utils.log.log  import main_log
+
+    for i in range(-30, 30):
+        a= random()*10**i
+        b=eng(a, 'Hz')
+        print(f'{a=} >> {b}')
+
     main_log()
-    
-    print(unit_converter('1000 Hz'))
-    print(unit_converter('0.0000001A'))
-    print(unit_converter('0.00001A')) 
-    print(unit_converter('1000000 Hz'))
-    print(unit_converter('50 A'))
-    print(unit_converter(1000))
-    print(unit_converter(0.0000001))
-    print(unit_converter(0.00001))
-    print(unit_converter(1000000))
-    print(unit_converter(50))
-    
+
     
         
