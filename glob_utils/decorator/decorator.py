9e5ebd9bdefedbd806551a67bd1943800262d8e4
David Metz <david.metz67@gmail.com>
2022-03-20 14:02:14 +0100

    fix decorator catch_error: nwo it return also what the function returns or None

--- glob_utils/decorator/decorator.py
+++ glob_utils/decorator/decorator.py
@@ -2,6 +2,7 @@
 
 import logging
 import traceback
+from typing import Any, Union
 import glob_utils.log.log
 
 logger = logging.getLogger(__name__)
@@ -10,14 +11,14 @@ logger = logging.getLogger(__name__)
 def catch_error(func):
     '''Decorator that reports the execution time.'''
   
-    def wrap(*args, **kwargs):
+    def wrap(*args, **kwargs)->Union[Any, None]:
         try:
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         except BaseException as e:
             msg= f'in {func.__name__}: {e}\n {traceback.format_exc()}'
             if glob_utils.log.log.check_logger_exist():
                 logger.error(msg)
             else:
                 print(f'Error {msg}')
-
+            return None
     return wrap
