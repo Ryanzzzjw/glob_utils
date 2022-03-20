a73110673b8fda7f2c029c17bd88f7d8d01b06fb
David Metz <david.metz67@gmail.com>
2022-03-20 14:04:51 +0100

    add dict_nested func

--- glob_utils/types/dict.py
+++ glob_utils/types/dict.py
@@ -1,4 +1,7 @@
 
+from typing import Any
+
+
 def visualise(d:dict,lvl:int=0, disp_val:bool=False)-> None:
     """print tree of the passed dict and nested dict
 
@@ -38,6 +41,8 @@ def visualise(d:dict,lvl:int=0, disp_val:bool=False)-> None:
     """
 
 
+
+
     # go through the dictionary alphabetically 
     for k in sorted(d):
 
@@ -47,7 +52,7 @@ def visualise(d:dict,lvl:int=0, disp_val:bool=False)-> None:
         if disp_val:
             val= str(d[k])
             header= '{:<25} {:<15} {:<10} {:30}'.format('KEY','LEVEL','TYPE', 'VAL')
-            line= "{:<25} {:<15} {:<10} {:30}".format(indent+str(k),lvl,t, val)
+            line= "{:<25} {:<15} {:<10} {:<20}".format(indent+str(k),lvl,t, val)
         else:
             header= '{:<25} {:<15} {:<10}'.format('KEY','LEVEL','TYPE')
             line="{:<25} {:<15} {:<10}".format(indent+str(k),lvl,t)
@@ -65,3 +70,31 @@ def visualise(d:dict,lvl:int=0, disp_val:bool=False)-> None:
         if type(d[k])==dict:
             # visualise THAT dictionary with +1 indent
             visualise(d[k],lvl+1)
+
+
+def dict_nested(obj:Any, ignore_private:bool=True)-> dict:
+    """ Return an object as a dictionary
+    an transform recursvely all object contained in obj..
+
+    Args:
+        obj (Any): _description_
+        return_private (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        dict: _description_
+    """
+    if not  hasattr(obj,"__dict__"):
+        return obj
+
+    result = {}
+    print(obj)
+    for key, val in obj.__dict__.items():
+            if key.startswith("_") and ignore_private:
+                continue
+            element = []
+            if isinstance(val, list):
+                element.extend(dict_nested(item) for item in val)
+            else:
+                element = dict_nested(val)
+            result[key] = element
+    return result
