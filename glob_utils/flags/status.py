2ae847c8ea3ad105e512377651bb185b8029217e
David Metz <david.metz67@gmail.com>
2022-03-23 17:41:59 +0100

    doc strings

--- glob_utils/flags/status.py
+++ glob_utils/flags/status.py
@@ -10,87 +10,180 @@ from glob_utils.thread_process.signal import Signal
 class BaseStatus(Enum):
     """"""
 
-
-class MultiState(object):
-    """Create and handle a Status, which can take multiple value.
-    """
+class StatusAgent(object):
+    """Agent which allow manage a Status, which can take multiple values.
     
+    e.g.:class Status(BaseStatus):
+                IDLE = 1
+                MEASURING = 1
+                PAUSED = 1
+
+    agent= StatusAgent(list(Status))
+
+    The first value in the status_values list will be took as 
+    initialisation value for the status.
+
+    the signal 'changed' will be emitted when the status value is changed
+    
+    Args:
+        status_values (list[Enum]): list of values which the 
+        status can take.
+    """
+    _status:Enum # Actual status value
+    _status_old:Enum # Last/older status value 
+    _status_values:list[Enum] # list of values which the status can take
+    changed: Signal # Signal is emited if the status value has been changed 
+
     def __init__(self, status_values:list[Enum]) -> None:
         super().__init__()
-        self.status_values = status_values
+        self.changed= Signal(self)
+        self._status_values = status_values
         self.reset()
         
-    def change_state(self, state:int):
-        """change the actual state"""
-        if state not in self.status_values:
-            raise ValueError(f'state should have the values {self.status_values}')
-        self.set_old(self.state)
-        self.state=state
-    
-    def is_set(self, state:int)->bool:
-        """Return if actual state is set to value"""
-        if state not in self.status_values:
-            raise ValueError(f'state should have the values {self.status_values}')
-        return self.state == state
-
-    def actual_state(self):
-        """Return value of the actual state"""
-        return self.state
-
-    def has_changed(self):
-        return self.state != self.state_old
-
-    def reset(self, state:int=None):
-        self.state_old = state
-        self.state = state
-
-    def ack_change(self):
-        self.set_old(self.state)
+    def change_status(self, val:Enum)->None:
+        """Change the actual status value and memory the last value
         
-    def set_old(self, state:int):
-        self.state_old = state
-    
-    def was_old(self, state:int)->bool:
-        """Return if older state was set to value"""
-        return self.state_old == state
-    
-    def was_set(self)->int:
-        """Return the older state """
-        return self.state_old
+        - memory older value
+        - set new status value
+        - emit changed signal
 
+        Args:
+            state (int): new status value
 
-class MultiStatewSignal(MultiState):
-    """Class responsible of creating and handling a flag (set, clear, is_set)"""
-    
-    def __init__(self,status_values:list[Enum]) -> None:
-        super().__init__(status_values)
-        self.changed= Signal(self)
-    
-    def change_state(self, state:Enum):
-        """change the actual state"""
-        if state not in self.status_values:
-            raise ValueError(f'state should have the values {self.status_values}')
-        self.set_old(self.actual_state())
-        self.state=state
-        self.fire_signals()
-    
-    def fire_signals(self):
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+        """
+        if val not in self._status_values:
+            raise ValueError(f'state should have the values {self._status_values}')
+        self.set_old(self.actual_status())
+        self._status=val
         if self.has_changed():
-            self.changed.fire()
-            a= MultiStatewSignal()
+            self.changed.emit()
+    
+    def is_set(self, val:Enum)->bool:
+        """Check if the actual status is set to val
+
+        Args:
+            val (Enum): status value to check
+
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+
+        Returns:
+            bool: return `True` if the actual status is set to val, 
+            `False` otherwise
+        """
+        self._check_val(val)
+        return self._status == val
+
+    def actual_status(self)->Enum:
+        """Return the actual status value
+
+        Returns:
+            Enum: value of actual status
+        """
+        return self._status
+
+    def has_changed(self)->bool:
+        """Asset if the status value changed since the last status change
+
+        Returns:
+            bool: `True` if the actual status differ from odler one,
+            otherwise `False`
+        """
+        return self._status != self._status_old
+
+    def reset(self, val:Enum=None)->None:
+        """Reset both actual and last status values to the passed value
+
+        Args:
+            val (Enum, optional): reset status value. if set to `None`, 
+            the first value in the _status_values list will be took as 
+            initialisation value for the status. Defaults to `None`.
+
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+        """
+        if val is None:
+            val= self._status_values[0]
+        self._check_val(val)
+        self._status_old = val
+        self._status = val
+        
+    def set_old(self, val:Enum)->None:
+        """Change the last/older status values
+
+        Args:
+            val (Enum): status value to set
+
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+        """
+        self._check_val(val)
+        self._status_old = val
+    
+    def was_old(self, val:Enum)->bool:
+        """Check if the status was set to val (check the last value of status)
+
+        Args:
+            val (Enum): status value to check
+
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+
+        Returns:
+            bool: return `True` if the last/older status was set to val, 
+            `False` otherwise
+        """
+        self._check_val(val)
+        return self._status_old == val
+    
+    def was_set(self)->Enum:
+        """Return the last/older status
+
+        Returns:
+            Enum: last/older status
+        """
+        return self._status_old
+    
+    def _check_val(self, val:Enum)->None:
+        """Check if the value is contained in the _status_values list
+        Raises:
+            ValueError: is raised if the value is not contained 
+            in the _status_values list
+        """
+        if val not in self._status_values:
+            raise ValueError(
+                f'state should have the values {self._status_values}'
+            )
+
 
 class StatusNotInitializated(BaseException):
     """"""
 
 class AddStatus(ABC):
-    _status: MultiStatewSignal
-    _was_status:Enum
+    """This Class allow to add a status to exixting obj and provide 
+    standard functionalities:
+
+    - `set_status` -> set the status to a new val
+    - `get_status` -> get actual status  
+    - `is_status`  -> test if actual status is set to val
+    - `reset_to_last_status` -> set the status to last (memorized) value
     
+    the abs-method `status_has_changed` is called when the status 
+    has been changed, and need to be defined in new instance. 
+
+    """
+    _status_agent: StatusAgent
         
     def __init__(self) -> None:
         super().__init__()
         
-
     def init_status(self, status_values:BaseStatus) -> None:
         """initialize the multistatus intern object
 
@@ -99,61 +192,58 @@ class AddStatus(ABC):
             hier the first enum should be the initial one
         """
         l= list(status_values)
-        self._status = MultiStatewSignal(l)
-        self._status.reset(l[0])
-        self._status.changed.connect(self._status_has_changed) 
+        self._status_agent = StatusAgent(l)
+        self._status_agent.reset(l[0])
+        self._status_agent.changed.connect(self._status_has_changed) 
     
     def _status_has_changed(self)->None:
         """Update the was life flag, called by status changed signal"""
-        self.status_has_changed(self._status.actual_state(), self._status.was_set())
+        self.status_has_changed(self._status_agent.actual_status(), self._status_agent.was_set())
 
     @abstractmethod
     def status_has_changed(self, status:Enum, was_status:Enum)->None:
         """This method is called wenn the status has been changed.
-        Implement here what has to be done wenn the status changed
+
+        Implement here what has to be done wenn the status changed!!
 
         Args:
-            status (_type_): actual status
-            was_status (_type_): older status
+            status (Enum): actual status
+            was_status (Enum): older status
         """
+    
     def emit_status_changed(self):
         self._status_has_changed()
 
     def is_status(self, val:Enum)->bool:
         self._check_is_init()
-        return self._status.is_set(val)
+        return self._status_agent.is_set(val)
     
     def get_status(self)->Enum:
         self._check_is_init()
-        return self._status.actual_state()
+        return self._status_agent.actual_status()
     
     def set_status(self, val:Enum)->None:
         self._check_is_init()
-        self._status.change_state(val)
+        self._status_agent.change_status(val)
 
-    def set_old_status(self)->None:
+    def reset_to_last_status(self)->None:
         self._check_is_init()
-        self._status.change_state(self._status.was_set())
+        self._status_agent.change_status(self._status_agent.was_set())
     
     def _check_is_init(self):
-        if self._status is None:
+        if self._status_agent is None:
             raise StatusNotInitializated(
                 'Please init the Status of the object before using it')
-   
-class testS(AddStatus):
 
-    def status_changed(self, status:Enum, was_status:Enum)-> None:
-        """"""
 
 if __name__ == "__main__":
     """"""
+    a= StatusAgent()
 
-    class AStates(BaseStatus):
+    class Status(BaseStatus):
         IDLE = 1
         MEASURING = 1
         PAUSED = 1
-    
-    a=testS(AStates)
 
 
     
