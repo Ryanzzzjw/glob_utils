e984c32355f5c3a27408f3302014675af7a3f539
David Metz <david.metz67@gmail.com>
2022-04-27 14:02:17 +0200

    add print np fro debugging

--- glob_utils/debug/debugging_help.py
+++ glob_utils/debug/debugging_help.py
@@ -4,6 +4,8 @@ import logging
 from typing import Any
 import logging
 
+from numpy import ndarray
+
 import glob_utils.log.log
 
 logger = logging.getLogger(__name__)
@@ -25,6 +27,17 @@ def print_obj_type_dict(obj:Any)->None:
         glob_utils.log.log.change_level_logging(level_tmp)
     else:
         print(msg)
+
+def print_np(array:ndarray, shape_only= False, id="array"):
+
+    msg= f'{id}.shape:{array.shape}' if shape_only else f'{id}:{array}\n{id}.shape:{array.shape}\ntype({id}):{type(array)}'
+    if glob_utils.log.log.check_logger_exist():
+        level_tmp= glob_utils.log.log.change_level_logging()
+        logger.debug(msg)
+        glob_utils.log.log.change_level_logging(level_tmp)
+    else:
+        print(msg)
+    
 
 
 if __name__ == '__main__':
